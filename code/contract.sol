4724b7d1be81038bbdf3f2b5f59c7782b1cb12e4
Júlio Igor <casemisto@gmail.com>
2023-12-05 17:12:36 -0300

    muda restauração para um array

--- code/contract.sol
+++ code/contract.sol
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.0;
 
 contract ArtMarketplace {
+    struct Restauration {
+        string description;
+        address payable restaurer;
+        bool isCompleted;
+        uint256 date;
+    }
+
     struct Art {
         string name;
         string description;
         uint256 price;
         address payable owner;
         bool isSold;
-        uint restaurationId;
-    }
-
-    struct Restauration {
-        string description;
-        address payable restaurer;
-        bool isCompleted;
+        uint256 date;
+        Restauration[] restaurations;
     }
 
     struct Restaurer {
@@ -23,15 +25,14 @@ contract ArtMarketplace {
     }
 
     Art[] public arts;
-    Restauration[] public restaurations;
     Restaurer[] public restaurers;
 
-    function mintArt(string memory _name, string memory _description, uint256 _price) public {
-        arts.push(Art(_name, _description, _price, payable(msg.sender), false, 0));
+    function mintArt(string memory _name, string memory _description, uint256 _price, uint256 _date) public {
+        arts.push(Art(_name, _description, _price, payable(msg.sender), false, _date, new Restauration[](0)));
     }
 
     function buyArt(uint256 _artId) public payable {
-        Art memory _art = arts[_artId];
+        Art storage _art = arts[_artId];
 
         require(msg.value >= _art.price, "Not enough Ether provided.");
         require(!_art.isSold, "Art piece already sold.");
@@ -39,31 +40,23 @@ contract ArtMarketplace {
         _art.owner.transfer(msg.value);
         _art.owner = payable(msg.sender);
         _art.isSold = true;
-
-        arts[_artId] = _art;
     }
 
-    function startRestauration(uint256 _artId, uint256 _restaurerId, string memory _description) public {
-        Art memory _art = arts[_artId];
+    function startRestauration(uint256 _artId, uint256 _restaurerId, string memory _description, uint256 _date) public {
+        Art storage _art = arts[_artId];
         Restaurer memory _restaurer = restaurers[_restaurerId];
 
         require(msg.sender == _art.owner, "Only the owner can start the restauration.");
-        require(_art.restaurationId == 0, "Art is already under restauration.");
 
-        _art.restaurationId = restaurations.length;
-        restaurations.push(Restauration(_description, _restaurer.account, false));
-
-        arts[_artId] = _art;
+        _art.restaurations.push(Restauration(_description, _restaurer.account, false, _date));
     }
 
-    function completeRestauration(uint256 _artId) public {
-        Art memory _art = arts[_artId];
-        Restauration memory _restauration = restaurations[_art.restaurationId];
+    function completeRestauration(uint256 _artId, uint256 _restaurationId) public {
+        Art storage _art = arts[_artId];
+        Restauration storage _restauration = _art.restaurations[_restaurationId];
 
         require(msg.sender == _restauration.restaurer, "Only the assigned restaurer can complete the restauration.");
 
         _restauration.isCompleted = true;
-
-        restaurations[_art.restaurationId] = _restauration;
     }
 }
